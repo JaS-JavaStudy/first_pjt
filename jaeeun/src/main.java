d76d083d809f81f96c506c4e95252b6d4ef4f57d
yunjaeeun <yunjaeeun12@naver.com>
2024-10-18 17:33:28 +0900

    24.10.18 윤재은 1. 기능별 메소드 분리 2. 두음법칙 추가 예정

--- jaeeun/src/main.java
+++ jaeeun/src/main.java
@@ -3,38 +3,33 @@ import java.util.Scanner;
 
 public class main {
     static Scanner sc = new Scanner(System.in);
+    static String before = "";
+    static int turn = 0;
 
     public static void main(String[] args) {
         System.out.print("사용자의 수를 입력해주세요: ");
         int playerNum = sc.nextInt();
         sc.nextLine();
 
-        String[] playerList = new String[playerNum];
-
-        System.out.println();
-        System.out.println("플레이어명은 영어로만 입력해주세요!");
-        for (int i = 0; i < playerNum; i++) {
-            System.out.print((i + 1) + "번째 플레이어: ");
-            playerList[i] = sc.nextLine();
-        }
-
         int[] points = new int[playerNum];
-        String before = "";
-        int turn = 0;
+        String[] playerList = setPlayer(playerNum);
+
         while (true) {
             if (turn == playerNum) {
                 turn = 0;
             }
 
             System.out.println();
-            System.out.print(playerList[turn] +"(" + points[turn] + ")" + " 님의 차례: ");
+            System.out.print(playerList[turn] + "(" + points[turn] + ")" + " 님의 차례: ");
             String word = sc.nextLine();
+
             if (before.equals("")) {
                 before = word;
                 points[turn] += word.length();
             } else {
-                int before_len = before.length();
-                if (word.charAt(0) == (before.charAt(before_len - 1))) {
+                boolean isVaild = validCheck(word);
+
+                if (isVaild) {
                     before = word;
                     points[turn] += word.length();
                 } else {
@@ -46,6 +41,34 @@ public class main {
             turn++;
         }
         System.out.println();
+
+        findWinner(playerNum, playerList, points);
+
+    }
+
+
+    private static String[] setPlayer(int playerNum) {
+        String[] playerList = new String[playerNum];
+
+        System.out.println();
+        System.out.println("플레이어명은 영어로만 입력해주세요!");
+        for (int i = 0; i < playerNum; i++) {
+            System.out.print((i + 1) + "번째 플레이어: ");
+            playerList[i] = sc.nextLine();
+        }
+
+        return playerList;
+    }
+
+    private static boolean validCheck(String word) {
+        int before_len = before.length();
+        if (word.charAt(0) == (before.charAt(before_len - 1))) {
+            return true;
+        }
+        return false;
+    }
+
+    private static void findWinner(int playerNum, String[] playerList, int[] points) {
         int max_point = 0;
         int max_idx = 0;
         for (int i = 0; i < playerNum; i++) {
