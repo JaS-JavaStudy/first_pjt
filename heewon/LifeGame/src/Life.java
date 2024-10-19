cdb7a87ab32e52a2dd9404feecd3d7989b8c8275
shinheewon0107 <shinheewon0107@gmail.com>
2024-10-19 12:42:26 +0900

    인생이란

--- heewon/LifeGame/src/Life.java
+++ heewon/LifeGame/src/Life.java
@@ -2,6 +2,19 @@ public class Life {
 
     public void first() {
         System.out.println("첫 번째 질문입니다. \n다음 중 하나를 선택하고 정수를 입력하세요. ex)2");
-        System.out.println("1 : 사랑  2 : 우정  3 : 명예  4 : Money");
+        System.out.println("가장 마음에 드는 동물을 고르세요.");
+        System.out.println("1 : 강아지  2 : 늑대  3 : 사자  4 : 돼지");
+    }
+    public void second() {
+        System.out.println("금요일에 먹고 싶은 음식을 고르세요.");
+        System.out.println("1 : 초콜렛  2 : 국밥  3 : 와인  4 : 초밥");
+    }
+    public void third() {
+        System.out.println("가장 마음에 드는 대답을 고르세요.");
+        System.out.println("1 : 웅!  2 : 엉  3 : 넵  4 : 입금 했어");
+    }
+    public void fourth() {
+        System.out.println("물에 아래의 사람들이 빠져있고 한명만 구할 수 있습니다. 누구를 구하시겠습니까?\n(단, 모두 수영을 못합니다.)");
+        System.out.println("1 : 남친 혹은 여친 혹은 썸남 혹은 썸녀 2 : 25년 된 친구  3 : 내 회사 대표 이사  4 : 일론 머스크");
     }
 }
