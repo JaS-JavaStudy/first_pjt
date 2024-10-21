d0e7f8873661c23fec5ec895e8232d9f59a91574
mincheol <kmc4889@naver.com>
2024-10-21 16:12:56 +0900

    1021_get_api수정본

--- jaeeun/src/main/java/GetApi.java
+++ jaeeun/src/main/java/GetApi.java
@@ -1,51 +1,59 @@
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.apache.http.client.utils.URIBuilder;
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class GetApi {
+    public GetApi() {
+    }
 
-    public static void main(String[] args) {
-
-                try {
-                    String clientId = myapi.clientId; //api
-                    String wordToSearch = "이름"; // 검색할 단어
-
-                    URIBuilder builder = new URIBuilder("https://opendict.korean.go.kr/api/search")
-                            .setParameter("certkey_no", "6977")
-                            .setParameter("key", clientId)
-                            .setParameter("target_type", "search")
-                            .setParameter("req_type", "json")
-                            .setParameter("part", "word")
-                            .setParameter("q", wordToSearch)
-                            .setParameter("sort", "dict")
-                            .setParameter("start", "1")
-                            .setParameter("num", "10");
-
-                    String url = builder.build().toString();
-                    System.out.println(url); // 최종 URL 출력
-
-                    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
-                        HttpGet request = new HttpGet(url);
-                        HttpResponse response = httpClient.execute(request);
-
-                        // 응답 처리
-                        if (response.getStatusLine().getStatusCode() == 200) {
-                            String jsonResponse = EntityUtils.toString(response.getEntity());
-                            logger.info("Response: {}", jsonResponse); // 결과 출력
-                        } else {
-                            logger.error("Error: HTTP status code {}", response.getStatusLine().getStatusCode());
-                        }
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+    public boolean isWordExist(String word) {
+        try {
+            String key = myapi.clientId;
+            String url = "https://opendict.korean.go.kr/api/search?certkey_no=6977";
+            url = url + "&key=" + key;
+            url = url + "&req_type=xml";
+            url = url + "&target_type=search";
+            url = url + "&part=word";
+            url = url + "&q=" + word;
+            url = url + "&sort=dict";
+            url = url + "&start=1&num=10";
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(url);
+            NodeList nl = doc.getElementsByTagName("item");
+            if (nl.getLength() == 0) {
+                System.out.println("'" + word + "'란 단어는 없습니다.");
+                return false;
+            } else {
+                Node node = nl.item(0);
+                if (node.getNodeType() == 1) {
+                    Element element = (Element)node;
+                    System.out.println("word : " + this.getValue("word", element));
+                    System.out.println("pos : " + this.getValue("pos", element));
+                    System.out.println("definition : " + this.getValue("definition", element));
+                    System.out.println();
                 }
+
+                return true;
             }
+        } catch (Exception var10) {
+            Exception e = var10;
+            e.printStackTrace();
+            return false;
         }
+    }
 
-
+    public String getValue(String tag, Element element) {
+        NodeList nl = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node value = nl.item(0);
+        return value.getNodeValue();
+    }
 }
